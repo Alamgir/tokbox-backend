f75b98f384027e54bf649dfca623fe7ad6b6857f
Alamgir Mand <alamgir@cboxgames.com>
2012-07-16 13:16:17 -0700

    added neo4j integration, working databse with user index

--- backend/src/com/tokbox/listener/OAuthListener.java
+++ backend/src/com/tokbox/listener/OAuthListener.java
@@ -1,9 +1,16 @@
 package com.tokbox.listener;
 
 import com.tokbox.service.TokBoxOAuth;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.rest.graphdb.ExecutingRestAPI;
+import org.neo4j.rest.graphdb.GraphDatabaseFactory;
+import org.neo4j.rest.graphdb.RestAPI;
+import org.neo4j.rest.graphdb.RestAPIFacade;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.sql.ClientInfoStatus;
 
 /**
  * Created by IntelliJ IDEA.
